96c9c7d5ae28fe2ceb6212093525743191027b21
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-07-14 22:40:51 +0200

    chore: add two favorite stops

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -2,4 +2,18 @@
 export const tokenEndPoint = "https://api.vasttrafik.se/token";
 export const VT_KEY = "vt";
 export const EMPTYJSON = "{}";
-export const ONE_HOUR = 1000 * 60 * 60;
+export const ONE_MINUTE = 1000 * 60;
+export const FJALLGATAN = {
+  id: "9021014002370000",
+  idx: "1",
+  lat: "57.694450",
+  lon: "11.936228",
+  name: "Fjällgatan, Göteborg"
+};
+export const STIGBERGSTORGET = {
+  id: "9021014006260000",
+  idx: "1",
+  lat: "57.698971",
+  lon: "11.935005",
+  name: "Stigbergstorget, Göteborg"
+};
