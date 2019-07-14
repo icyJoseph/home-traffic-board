8ba04e510bfe9d13d7a2ef54f22d0cc241c03ae1
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-07-14 22:43:08 +0200

    feat: check token validity every minute

--- src/ui/context/auth.tsx
+++ src/ui/context/auth.tsx
@@ -1,11 +1,12 @@
 import React, { createContext, useEffect, useContext, useReducer } from "react";
 import axios from "axios";
 import { getToken } from "../../data/token";
-import { ONE_HOUR } from "../../data/constants";
+import { ONE_MINUTE } from "../../data/constants";
 
 interface IAuthContext {
   token: Token;
   error: boolean;
+  loading: boolean;
 }
 
 interface IAuthProvider {
@@ -46,7 +47,7 @@ type AuthActionsTypes = FetchToken | SuccessToken | FailureToken;
 const authReducer = (state: AuthState, action: AuthActionsTypes): AuthState => {
   switch (action.type) {
     case AuthActions.FETCH:
-      return { ...state, loading: true, error: false };
+      return { ...authInitialState, loading: true };
     case AuthActions.FAILURE:
       return { ...authInitialState, error: true };
     case AuthActions.SUCCESS:
@@ -62,11 +63,15 @@ const authReducer = (state: AuthState, action: AuthActionsTypes): AuthState => {
   }
 };
 
-const AuthContext = createContext<IAuthContext>({ token: "", error: false });
+const AuthContext = createContext<IAuthContext>({
+  token: "",
+  error: false,
+  loading: false
+});
 
 export function AuthProvider({ children }: IAuthProvider) {
   const [state, dispatch] = useReducer(authReducer, authInitialState);
-  const { error, token, expiry } = state;
+  const { error, token, loading, expiry } = state;
 
   // check token
   useEffect(() => {
@@ -89,13 +94,23 @@ export function AuthProvider({ children }: IAuthProvider) {
   }, [token]);
 
   // at intervals check token validity
+  // implement as follows: after one minute => check if still valid
+  // if valid, do nothing, otherwise fetch token
+
   useEffect(() => {
-    const interval = setInterval(() => expiry > new Date().getTime(), ONE_HOUR);
-    return () => clearInterval(interval);
+    const verifyToken = () => {
+      const expired = new Date().getTime() > expiry;
+      if (expired) {
+        dispatch({ type: AuthActions.FETCH }); // token is set to ""
+      }
+      return () => {}; //noop
+    };
+    const timer = setTimeout(verifyToken, ONE_MINUTE);
+    return () => clearTimeout(timer);
   }, [expiry]);
 
   return (
-    <AuthContext.Provider value={{ token, error }}>
+    <AuthContext.Provider value={{ token, error, loading }}>
       {children}
     </AuthContext.Provider>
   );
