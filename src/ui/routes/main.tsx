12d274b04329a0ab5a33fec0eca9fe9501c89308
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-07-21 00:47:17 +0200

    feat: remove search input temporarily

--- src/ui/routes/main.tsx
+++ src/ui/routes/main.tsx
@@ -2,22 +2,20 @@ import React from "react";
 import { useAuth } from "../context/auth";
 
 import Time from "../components/Time";
-import Search from "../components/Search";
+// import Search from "../components/Search";
 import Board from "../components/Board";
 
 const App: React.FC = () => {
   const { loading, error } = useAuth();
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
         {loading ? <p>Loading Token</p> : null}
         {error && <p>Error Getting Token</p>}
-      </header>
-      <section>
         <Time />
-      </section>
-      {!error ? <Search /> : null}
+      </header>
+      {/* {!error ? <Search /> : null} */}
       {!error ? <Board /> : null}
     </div>
   );
